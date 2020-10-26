b06e63f2ba882a32bc0de235d01318d148bbc57e
box3101 <box4101@nate.com>
2020-10-26 16:15:51 +0900

    작업내용 커밋

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -6,6 +6,24 @@
     loop: true,
     autoplay: true,
     speed: 1000,
+
+    breakpoints: {
+      // when window width is >= 320px
+      1200: {
+        slidesPerView: 6,
+        spaceBetween: 20,
+      },
+      // when window width is >= 480px
+      900: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+      // when window width is >= 640px
+      600: {
+        slidesPerView: 2.5,
+        spaceBetween: 10,
+      },
+    },
   });
 
   // faq 슬라이드
@@ -17,7 +35,47 @@
 
     navigation: {
       nextEl: ".com-slide__button--next", // 다음 버튼 클래스명
-      prevEl: ".com-slide__button--prev", // 이번 버튼 클래스명
+      prevEl: ".com-slide__button--prebv", // 이번 버튼 클래스명
     },
+  });
+})();
+
+// TODO : header fixed 스크립트
+(function () {
+  function headerFixed() {
+    const hdWrap = document.querySelector(".header");
+    const logoImg = document.querySelector(".logo-img");
+
+    let scroll = window.pageYOffset;
+
+    if (scroll > 0) {
+      hdWrap.classList.add("is-fix");
+      logoImg.setAttribute("src", "../assets/images/img/logo-fix.svg");
+    } else {
+      hdWrap.classList.remove("is-fix");
+    }
+  }
+
+  window.addEventListener("scroll", function () {
+    headerFixed();
+  });
+})();
+
+//TODO 메인 스크롤 모션 (what do)
+(function () {
+  function WhatDoEvent() {
+    let scroll = window.pageYOffset;
+    const whatDO = document.querySelector(".what-do");
+
+    // 스크롤시 scle 커짐
+    if (scroll > 0) {
+      whatDO.classList.add("active");
+    } else {
+      whatDO.classList.remove("active");
+    }
+  }
+
+  window.addEventListener("scroll", function () {
+    WhatDoEvent();
   });
 })();
